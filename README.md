6a141a95feea9448de2c6430aca04f7c31948dbf
steffilazerte <steffi@steffi.ca>
2023-01-19 10:06:13 -0600

    Add cmdstanr and rnaturalearthhires

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ This is the setup for the bbsBayes [R-Universe](https://bbsBayes.r-universe.dev)
 
 Instructions can be found in the article, 
 ["How to create your personal CRAN-like repository on R-universe"](https://ropensci.org/blog/2021/06/22/setup-runiverse/)
+
+
+We also include rnaturalearthhires and cmdstanr in order to have those packages
+available for building.
--- universe_files.R
+++ universe_files.R
@@ -3,7 +3,9 @@
 library(dplyr)
 
 df <- tribble(
-  ~package,                  ~url,
-  "bbsBayes2",            "https://github.com/bbsBayes/bbsBayes2")
+  ~package,            ~url,                                              ~branch,
+  "bbsBayes2",          "https://github.com/bbsBayes/bbsBayes2",          "*release", # Track most recent release
+  "cmdstanr",           "https://github.com/stan-dev/cmdstanr/",          "*release", # Track most recent release
+  "rnaturalearthhires", "https://github.com/ropensci/rnaturalearthhires", NA_character_)
 
 jsonlite::write_json(df, 'packages.json', pretty = TRUE)
